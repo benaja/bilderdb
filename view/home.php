c9379d27edffe25304842c34e9de9cd5c1743819
noahporfido <noah.porfido@icloud.com>
2018-03-15 14:31:06 +0100

    home page styling
    added heading and styling for sign in

--- view/home.php
+++ view/home.php
@@ -1,2 +1,19 @@
 <div class='home-container'>
+    <div class="title-container">
+        <h2>PictureCloud</h2>
+    </div>
+    <div class="login-container">
+        <form class="login-form">
+            <input type="email" placeholder="E-Mail">
+            <input placeholder="Password" class="valdiate" type="password">
+            <div class="login-button-container">
+                <input type="submit" value="Login" class="login-button">
+                <img class="arrow-right" src="/images/arrow-right.png">
+            </div>
+        </form>
+        <div class="signup-button-container">
+            <button class="login-button">Sign up</button>
+            <img class="arrow-right" src="/images/arrow-right.png">
+        </div>
+    </div>
 </div>
