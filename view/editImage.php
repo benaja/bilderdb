93087c335c8db1c9f226cca0cf56b1124d4b16ae
noahporfido <noah.porfido@icloud.com>
2018-05-17 14:21:34 +0200

    edit page inProgress

---
+++ view/editImage.php
@@ -0,0 +1,13 @@
+<div class="edit-image-container">
+    <form method="POST" action="/gallery">
+        <div class="input-field col s6">
+            <input id="last_name" type="text" class="validate">
+            <label for="last_name">Name</label>
+        </div>
+        <div class="input-field col s6">
+            <input id="last_name" type="text" class="validate">
+            <label for="last_name">Description</label>
+        </div>
+        <a class="btn-large" type="submit">Save</a>
+    </form>
+</div>
